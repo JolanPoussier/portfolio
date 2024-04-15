467e61dee9a3ae3b523ce0e5a9cfc41df90330fb
JolanPoussier <jojopoussier@gmail.com>
2024-04-15 15:25:00 +0200

    feat: home -logo -navigation -presentation

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,27 +1,72 @@
+import { ChevronDown } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 import styled from 'styled-components';
 
 const Main = styled.div`
   height: 100%;
+  width: 80%;
+  margin: auto;
   display: flex;
-  alignt-itms: center;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.div`
+  display: flex;
   justify-content: center;
   flex-direction: column;
 `;
-const Intro = styled.h1`
-  font-family: ${(props) => props.theme.fonts.bold};
-  font-size: 4em;
+const Name = styled.h1`
+  font-family: ${(props) => props.theme.fonts.extralight};
+  font-size: 2em;
+  margin-bottom: 1em;
 `;
-
 const Job = styled.h1`
   font-family: ${(props) => props.theme.fonts.bold};
   font-size: 6em;
 `;
 
+const Picture = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 4em;
+  margin-left: 4em;
+  border-radius: 20px;
+  overflow: hidden;
+`;
+
+const BottomLink = styled(Link)`
+  position: absolute;
+  bottom: 48px;
+  color: grey;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: white;
+  }
+`;
+
 export default function Home() {
   return (
     <Main>
-      <Intro>Jolan Poussier</Intro>
-      <Job>Web Developper</Job>
+      <Text>
+        <Name>Jolan Poussier</Name>
+        <Job>
+          Web <br /> Developer
+        </Job>
+      </Text>
+      <Picture>
+        <Image
+          width={408}
+          height={306}
+          src={'/assets/profilPic.png'}
+          alt="Handsom young man with long hair, a very charismatic face and an enigmatic smile"
+        />
+      </Picture>
+      <BottomLink href={'/#projects'}>
+        <ChevronDown strokeWidth={1} size={48} />
+      </BottomLink>
     </Main>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Github, Linkedin } from 'lucide-react';
 import Link from 'next/link';
 import styled from 'styled-components';
 
@@ -21,6 +22,16 @@ const Container = styled.header`
   font-size: 18px;
 `;
 
+const ContactSection = styled.div`
+  position: fixed;
+  top: 35px;
+  right: 24px;
+`;
+
+const ContactLink = styled(Link)`
+  margin-left: 24px;
+`;
+
 const NavLink = styled(Link)<{ focus?: string }>`
   height: 100%;
   width: 100px;
@@ -36,28 +47,41 @@ const NavLink = styled(Link)<{ focus?: string }>`
 
 export default function Navbar({ location }: { location: string }) {
   return (
-    <Container>
-      <NavLink
-        focus={location == '' || location == 'home' ? 'true' : ''}
-        href={'/#home'}
-        scroll={true}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        focus={location == 'projects' ? 'true' : ''}
-        href={'/#projects'}
-        scroll={true}
-      >
-        Projects
-      </NavLink>
-      <NavLink
-        focus={location == 'about' ? 'true' : ''}
-        href={'/#about'}
-        scroll={true}
-      >
-        About me
-      </NavLink>
-    </Container>
+    <>
+      <Container>
+        <NavLink
+          focus={location == '' || location == 'home' ? 'true' : ''}
+          href={'/#home'}
+          scroll={true}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          focus={location == 'projects' ? 'true' : ''}
+          href={'/#projects'}
+          scroll={true}
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          focus={location == 'about' ? 'true' : ''}
+          href={'/#about'}
+          scroll={true}
+        >
+          About me
+        </NavLink>
+      </Container>
+      <ContactSection>
+        <ContactLink target="_blank" href={'https://github.com/JolanPoussier'}>
+          <Github strokeWidth={1.2} />
+        </ContactLink>
+        <ContactLink
+          target="_blank"
+          href={'https://www.linkedin.com/in/jolan-poussier/'}
+        >
+          <Linkedin strokeWidth={1.2} />
+        </ContactLink>
+      </ContactSection>
+    </>
   );
 }
